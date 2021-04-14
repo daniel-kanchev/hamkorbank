1a7e1599f254b2839368953d2a6eb6cb17994018
daniel-kanchev <daniel.kanchev@adata.pro>
2021-04-14 14:11:39 +0300

    Finished Script

---
+++ README.md
@@ -0,0 +1,15 @@
+URL: https://hamkorbank.uz/uz/press-center/news/
+
+    Spider name: hamkorbank
+
+DB Schema:
+- title
+- date
+- link
+- content
+
+Time: 159.88s
+
+Number of articles: 653
+
+
---
+++ hamkorbank/settings.py
@@ -0,0 +1,12 @@
+BOT_NAME = 'hamkorbank'
+SPIDER_MODULES = ['hamkorbank.spiders']
+NEWSPIDER_MODULE = 'hamkorbank.spiders'
+USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
+ITEM_PIPELINES = {
+    'hamkorbank.pipelines.DatabasePipeline': 300,
+}
+FEED_EXPORT_ENCODING = 'utf-8'
+
+LOG_LEVEL = 'WARNING'
+
+# LOG_LEVEL = 'DEBUG'
